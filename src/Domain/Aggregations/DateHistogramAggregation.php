4ea241a05e9148385234423809dc90b9c7f8d549
fbs <f.beck-klaus@bena-service.de>
2022-04-11 22:45:03 +0200

    add date histogram aggregation

---
+++ src/Domain/Aggregations/DateHistogramAggregation.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace JeroenG\Explorer\Domain\Aggregations;
+
+
+final class DateHistogramAggregation implements AggregationSyntaxInterface
+{
+
+
+    private string $timezone;
+    private string $interval;
+    private string $field;
+    private array $options;
+
+    public function __construct(string $field, string $interval, string $timezone, array $options = [])
+    {
+        $this->field = $field;
+        $this->interval = $interval;
+        $this->options = $options;
+        $this->timezone = $timezone;
+    }
+
+    public function build(): array
+    {
+
+        return [
+            "date_histogram" => [
+                "min_doc_count" => 0,
+                "field" => $this->field,
+                "fixed_interval" => $this->interval,
+                "time_zone" => $this->timezone,
+                ...$this->options
+            ]
+
+        ];
+    }
+}
